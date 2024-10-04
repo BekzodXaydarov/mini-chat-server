8e5b97a3f1f70c00094c9b91f9cc7550c3f6e1d4
Takitok2011 <bekzodeoke@gmail.com>
2024-10-04 22:28:54 +0500

    m

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,5 +6,6 @@ const upload = require("../config/multer")
 
 router.post("/user",upload.single("photo"),UserController.createUser)
 router.get("/user",UserController.getUser)
+router.get("/user/:id",UserController.getUserById)
 
 module.exports = router
